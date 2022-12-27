45031aba8d3b48419dc283c7cbe1f2dc4c23f35c
Bryon F <bryon790@revature.net>
2022-12-27 16:33:58 -0600

    added Context.cs and IContext.cs

---
+++ ECommerce.Data/IContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ECommerce.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Data
+{
+    public interface IContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public void DenoteProductModified(Product product);
+        public void DenoteUserModified(User user);
+
+        public Task CommitChangesAsync();
+    }
+}
