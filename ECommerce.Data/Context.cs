2bd0bc6c56e2ad4008e12f2a9e523b5f446c1470
Brianna Renni <briannaxrene@icloud.com>
2022-12-31 22:17:11 -0800

    Add auth xunit tests

--- ECommerce.Data/Context.cs
+++ ECommerce.Data/Context.cs
@@ -3,8 +3,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Core;
 using Microsoft.EntityFrameworkCore;
-
-
 //using System.Data.Entity;
 //using System.Data.Entity;
 
@@ -13,13 +11,9 @@ namespace ECommerce.Data
     public class Context : DbContext, IContext
     {
 
-        public Context(DbContextOptions<Context> options) : base(options)
-        {
+        public Context(DbContextOptions<Context> options) : base(options) {}
 
-
-
-        }
-
+        public Context() {}
 
         public DbSet<Product> Products { get; set; } = null!;
         public DbSet<User> Users { get; set; } = null!;
@@ -57,7 +51,6 @@ namespace ECommerce.Data
         }
 
         /**User Table**/
-
         public async Task<User?> GetUserById(int id)
         {
             return await Users.FindAsync(id);
@@ -79,33 +72,11 @@ namespace ECommerce.Data
             }
         }
 
-
         public async Task<User?> GetUserByEmailAndPassword(string email, string password)
         {
             email.Trim();
             password.Trim();
             return await Users.FirstOrDefaultAsync(u => u.email == email && u.password == password);
         }
-
-        // public async Task<bool> GetUserLogin(User usr)
-        // {
-        //     return Users.Any(u => u.email == usr.email && u.password == usr.password);
-        // }
-
-
-
-        // public async Task<User> GetUserLogin(string email, string password)
-        // {
-        //     var user = Users.Where(usr => usr.email == email && usr.password == password).FirstOrDefault();
-
-        //     if (user == null)
-        //     {
-        //         throw new Exception("Invalid login information");
-        //     }
-
-        //     return user;
-        // }
-
     }
-
 }
