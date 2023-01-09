537a1fecf2c84b92a4ef432f01e8df713f1f31fa
Brianna Renni <briannaxrene@icloud.com>
2023-01-09 07:37:55 -0800

    Clean up Program.cs, add startup.md

--- ECommerce.API/Program.cs
+++ ECommerce.API/Program.cs
@@ -12,44 +12,19 @@ builder.Services.AddCors(options =>
     options.AddDefaultPolicy(
         policy =>
         {
-            policy.WithOrigins("https://ecommerceappteamb.github.io/P3UI-TeamB",
-                               "https://ecommerceappteamb.github.io")//,"http://localhost:4200")
-
-                       .AllowAnyMethod()
-                       .AllowAnyHeader()
-                      .AllowCredentials();
-                   
+            policy.WithOrigins("https://ecommerceappteamb.github.io/P3UI-TeamB")
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
         });
 });
 
-//builder.Services.AddSingleton<IRepository>
-//    (sp => new SQLRepository(connString, sp.GetRequiredService<ILogger<SQLRepository>>()));
-
 builder.Services.AddControllers();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-//Emtity framework chage -- Bryon
 builder.Services.AddDbContext<Context>(opt => opt.UseSqlServer(connString));
 builder.Services.AddScoped<IContext>(provider => provider.GetService<Context>());
-
-//var githubapp = "_GithubAppP3TB";
-//builder.Services.AddCors(options =>
-//{
-//    options.AddPolicy(name: githubapp,
-//                      policy =>
-//                      {
-//                       //P3UI-TeamB")policy.WithOrigins("https://ecommerceappteamb.github.io")
-//                        //  policy.AllowAnyOrigin()
-//                                .AllowAnyMethod()
-//                                .AllowAnyHeader()
-//                                .AllowCredentials();
-//                               // .WithExposedHeaders("Access-Control-Allow-Origin");
-
-//                      });
-//});
 
 var app = builder.Build();
 
@@ -63,30 +38,12 @@ if (app.Environment.IsDevelopment())
    });
 }
 
-//if (app.Environment.IsProduction())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI(options =>
-//    {
-//        options.SwaggerEndpoint("/swagger/v1/swagger.json", "EComm-API");
-//    });
-//}
-
 app.UseHttpsRedirection();
 
-// second option
-/*app.UseCors(policy => policy.AllowAnyHeader()
-                            .AllowAnyMethod()
-                            //.AllowAnyOrigin()
-                            .SetIsOriginAllowed(origin => true)
-                            //.AllowCredentials()
-);*/
+app.UseCors();
 
 app.UseAuthorization();
 
 app.MapControllers();
-
-//app.UseCors(githubapp);
-app.UseCors();
 
 app.Run();
